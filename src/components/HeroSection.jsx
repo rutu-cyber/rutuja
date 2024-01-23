25814845a40ca7eca16ff8ddb68217a41eba54a0
rutu-cyber <rutujadevkar@gmail.com>
2024-01-23 20:49:00 +0530

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,68 @@
+"use client";
+import React from 'react';
+import Image from 'next/image';
+import { TypeAnimation } from 'react-type-animation';
+
+const HeroSection = () => {
+  return (
+    <section>
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className='col-span-7 place-self-center text-center sm:text-left'>
+                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+                        Hello, I&apos;m{" "}
+                    </span> 
+                    <br></br>
+                    <TypeAnimation
+                    sequence={[
+                        'Rutuja',
+                        1000, 
+                        'FullStack Developer',
+                        1000,
+                        'MERN Developer',
+                        1000,
+                        'Cloud Practitioner',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    />
+                </h1>
+                <p className='text-[#adb7be] text-base sm:text-lg mb-6 lg:text-xl'>
+                    Full stack software developer with expertise in front-end and back-end development. 
+                </p>
+                <div>
+                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>
+                        Hire Me
+                    </button>
+                    <button
+                      className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'
+                    >
+                      <a
+                        href='/images/cv.pdf'  // Update the file name accordingly
+                        download='cv.pdf'  // Update the desired downloaded file name
+                        className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'
+                      >
+                        Download CV
+                      </a>
+                    </button>
+                </div>
+            </div>
+            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
+                    <Image 
+                    src='/images/hero-image.png' 
+                    alt='hero image'
+                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                    width={300}
+                    height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection;
---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState, useRef } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
+
+const projectsData = [
+    {
+      id: 1,
+      title: "Crypto Hunter",
+      description: "Cryptocurrency Tracker with react and materialUI",
+      image: "/images/projects/1.png",
+      tag: ["All", "React"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 2,
+      title: "Admin Dashboard",
+      description: "React based admin dashboard",
+      image: "/images/projects/2.png",
+      tag: ["All", "React"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 3,
+      title: "Shopito",
+      description: "E-commerce Application, Authentication and Wallet",
+      image: "/images/projects/3.png",
+      tag: ["All", "MERN"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 4,
+      title: "Homzy",
+      description: "Real-estate full stack website",
+      image: "/images/projects/4.png",
+      tag: ["All", "MERN"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 5,
+      title: "Pokedex",
+      description: "Pokemon website with react and firebase",
+      image: "/images/projects/5.png",
+      tag: ["All", "React"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 6,
+      title: "Blog",
+      description: "Blog App",
+      image: "/images/projects/6.png",
+      tag: ["All", "React"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 7,
+      title: "Hotel Website",
+      description: "Next and React based website used stripe",
+      image: "/images/projects/4.png",
+      tag: ["All", "React"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+    {
+      id: 8,
+      title: "AI prompt Selling",
+      description: "Next and React based website used typescript",
+      image: "/images/projects/3.png",
+      tag: ["All", "MERN"],
+      gitUrl: "/",
+      previewUrl: "/",
+    },
+  ];
+
+  const ProjectsSection = () => {
+    const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+  
+    const handleTagChange = (newTag) => {
+      setTag(newTag);
+    };
+  
+    const filteredProjects = projectsData.filter((project) =>
+      project.tag.includes(tag)
+    );
+  
+    const cardVariants = {
+      initial: { y: 50, opacity: 0 },
+      animate: { y: 0, opacity: 1 },
+    };
+  
+    return (
+      <section id="projects">
+        <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
+          My Projects
+        </h2>
+        <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+          <ProjectTag
+            onClick={handleTagChange}
+            name="All"
+            isSelected={tag === "All"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="React"
+            isSelected={tag === "React"}
+          />
+          <ProjectTag
+            onClick={handleTagChange}
+            name="MERN"
+            isSelected={tag === "MERNy"}
+          />
+        </div>
+        <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+          {filteredProjects.map((project, index) => (
+            <motion.li
+              key={index}
+              variants={cardVariants}
+              initial="initial"
+              animate={isInView ? "animate" : "initial"}
+              transition={{ duration: 0.3, delay: index * 0.4 }}
+            >
+              <ProjectCard
+                key={project.id}
+                title={project.title}
+                description={project.description}
+                imgUrl={project.image}
+                gitUrl={project.gitUrl}
+                previewUrl={project.previewUrl}
+              />
+            </motion.li>
+          ))}
+        </ul>
+      </section>
+    );
+  };
+
+export default ProjectsSection
