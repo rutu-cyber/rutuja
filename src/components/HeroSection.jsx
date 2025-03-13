5ee72d08a8418b88fc4557f5def8df925cde67a1
rutu-cyber <rutujadevkar@gmail.com>
2025-03-13 15:36:59 +0530

    updated cv

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -38,12 +38,12 @@ const HeroSection = () => {
                       className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'
                     >
                       <a
-                        href='/images/cv.pdf'
+                        href='/images/RutujaDevkar_CV.pdf'
                         download='RutujaDevkar_CV.pdf'
                         className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'
                       >
                         Download CV
-                      </a>
+                      </a>    
                     </button>
                 </div>
             </div>
