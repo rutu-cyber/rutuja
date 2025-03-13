04a67d9e2a938f4c05aa0fa1fa83d36b30aea8f5
rutu-cyber <rutujadevkar@gmail.com>
2025-03-13 15:30:25 +0530

    new cv added

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -38,8 +38,8 @@ const HeroSection = () => {
                       className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'
                     >
                       <a
-                        href='/images/cv.pdf'  // Update the file name accordingly
-                        download='cv.pdf'  // Update the desired downloaded file name
+                        href='/images/cv.pdf'
+                        download='RutujaDevkar_CV.pdf'
                         className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'
                       >
                         Download CV
